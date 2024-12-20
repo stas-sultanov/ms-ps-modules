3fd03da8163521a83c3415c323acd6d2802aee93
Stas Sultanov <stas.sultanov@outlook.com>
2024-12-20 20:17:38 +0200

    up

--- sources/Helpers.psm1
+++ sources/Helpers.psm1
@@ -1,78 +1,221 @@
 using namespace System;
 
-class ConsoleLogHelper
-{
-	[ConsoleColor] $colorBorder = [ConsoleColor]::White;
-	[ConsoleColor] $colorFail = [ConsoleColor]::Red;
-	[ConsoleColor] $colorMessage = [ConsoleColor]::Blue;
-	[ConsoleColor] $colorSuccess = [ConsoleColor]::Green;
-
-	# pad right value for message allignment
-	[Int32] $messagePadRightWidth;
-
-	LogHelper (
-		[Int32] $messagePadRightWidth
-	)
-	{
-		$this.messagePadRightWidth = $messagePadRightWidth;
-	}
-
-	[Void] OperationBegin(
-		[String] $message
-	)
-	{
-		Write-Host '| ' -ForegroundColor $this.colorBorder -NoNewline;
-
-		Write-Host $message.PadRight($this.messagePadRightWidth) -ForegroundColor $this.colorMessage -NoNewline;
-
-		Write-Host '| ' -ForegroundColor $this.colorBorder -NoNewline;
-	}
-
-	[Void] OperationEnd(
-		[Boolean] $status,
-		[String] $message = $null
-	)
-	{
-		if ($status)
-		{
-			Write-Host 'Success'.PadRight(8) -ForegroundColor Green -NoNewline;
-		}
-		else
-		{
-			Write-Host 'Fail'.PadRight(8) -ForegroundColor Red -NoNewline;
-		}
-
-		if ([String]::IsNullOrWhiteSpace($message))
-		{
-			Write-Host ' |' -ForegroundColor White;
-		}
-		else
-		{
-			Write-Host ' | ' -ForegroundColor White -NoNewline;
-
-			Write-Host $message -ForegroundColor Green;
-		}
-	}
-
-	[Void] OperationEndSuccess(
-		[Nullable[String]] $message = $null
-	)
-	{
-		$this.OperationEnd($true, $message);
-	}
-}
-
-[OutputType[ConsoleLogHelper]]
 function New-ConsoleLogHelper
 {
+	<#
+	.SYNOPSIS
+		Create a new instance of ConsoleLogHelper class.
+	.DESCRIPTION
+		Helps use console for loggin.
+	.PARAMETER padWidthOperation
+		Message column padding.
+	.OUTPUTS
+		Instance of ConsoleLogHelper class.
+	.NOTES
+		Copyright © 2024 Stas Sultanov.
+	#>
+	[OutputType([ConsoleLogHelper])]
 	param
 	(
-		[Parameter(Mandatory = $true)] [Int32] $messagePadRightWidth
+		[Parameter(Mandatory = $true)] [Int32] $padWidthOperation
 	)
 	process
 	{
-		return [ConsoleLogHelper]::new($messagePadRightWidth);
+		$result = [ConsoleLogHelper]::new($padWidthOperation);
+
+		return $result;
 	}
 }
 
 Export-ModuleMember -Function New-ConsoleLogHelper;
+
+class ConsoleLogHelper
+{
+	[ConsoleColor] $colorTabelBorder = [ConsoleColor]::Blue;
+	[ConsoleColor] $colorTabelColumnName = [ConsoleColor]::Gray;
+	[ConsoleColor] $colorDetails = [ConsoleColor]::Yellow;
+	[ConsoleColor] $colorId = [ConsoleColor]::Magenta;
+	[ConsoleColor] $colorOperation = [ConsoleColor]::White;
+	[ConsoleColor] $colorStatusFail = [ConsoleColor]::Red;
+	[ConsoleColor] $colorStatusSuccess = [ConsoleColor]::Green;
+
+	hidden [String] $columnNameDetails = 'Details';
+	hidden [String] $columnNameId = 'Id';
+	hidden [String] $columnNameOperation = 'Operation';
+	hidden [String] $columnNameStatus = 'Status';
+	hidden [String] $columnNameTime = 'Time, ms';
+
+	hidden [DateTime] $operationBeginTime;
+	hidden [DateTime] $operationEndTime;
+	hidden [Int32] $operationCounter = 0;
+
+	hidden [Int32] $padWidthId = 2;
+	hidden [Int32] $padWidthOperation;
+	hidden [Int32] $padWidthStatus = 7;
+	hidden [Int32] $padWidthTime = 8;
+
+	hidden [String] $tableFillDetails;
+	hidden [String] $tableFillId;
+	hidden [String] $tableFillOperation;
+	hidden [String] $tableFillStatus;
+	hidden [String] $tableFillTime;
+
+	ConsoleLogHelper (
+		[Int32] $padWidthOperation
+	)
+	{
+		$this.padWidthOperation = $padWidthOperation;
+
+		$this.tableFillDetails = '━' * 10;
+		$this.tableFillId = '━' * ($this.padWidthId + 2);
+		$this.tableFillOperation = '━' * ($this.padWidthOperation + 2);
+		$this.tableFillStatus = '━' * ($this.padWidthStatus + 2);
+		$this.tableFillTime = '━' * ($this.padWidthTime + 2);
+	}
+
+	[Void] OperationBegin(
+		[String] $operation
+	)
+	{
+		# increment operation counter
+		$this.operationCounter++;
+
+		# write row begin
+		$this.WriteRowBegin(
+			$this.operationCounter.ToString().PadLeft($this.padWidthId),
+			$this.colorId,
+			$operation.PadRight($this.padWidthOperation),
+			$this.colorOperation
+		);
+
+		# set begin time
+		$this.operationBeginTime = [DateTime]::UtcNow;
+	}
+
+	[Void] OperationEnd(
+		[Boolean] $success,
+		[String] $details
+	)
+	{
+		# set begin time
+		$this.operationEndTime = [DateTime]::UtcNow;
+
+		# calculate opretaion time
+		$operationTime = [Int32] ($this.operationEndTime.Subtract($this.operationBeginTime).TotalMilliseconds);
+
+		# status
+		$status = ($success ? 'Success' : 'Fail').PadRight($this.padWidthStatus);
+
+		$statusColor = $success ? $this.colorStatusSuccess : $this.colorStatusFail;
+
+		$this.WriteRowEnd(
+			$status,
+			$statusColor,
+			$operationTime.ToString().PadLeft($this.padWidthTime),
+			$this.colorOperation,
+			$details,
+			$this.colorDetails
+		);
+	}
+
+	[Void] OperationEnd(
+		[Boolean] $status
+	)
+	{
+		$this.OperationEnd($status, $null);
+	}
+
+	[Void] OperationEndSuccess(
+	)
+	{
+		$this.OperationEnd($true, $null);
+	}
+
+	[Void] OperationEndSuccess(
+		[String] $message
+	)
+	{
+		$this.OperationEnd($true, $message);
+	}
+
+	[Void] ProcessBegin(
+	)
+	{
+		$this.operationCounter = 0;
+
+		# table head top
+
+		$tableHeadTop = '┏' + $this.tableFillId + '┳' + $this.tableFillOperation + '┳' + $this.tableFillStatus + '┳' + $this.tableFillTime + '┳' + $this.tableFillDetails;
+
+		Write-Host $tableHeadTop -ForegroundColor $this.colorTabelBorder;
+
+		# teble column names
+		$this.WriteRowBegin(
+			$this.columnNameId.PadLeft($this.padWidthId),
+			$this.colorTabelColumnName,
+			$this.columnNameOperation.PadRight($this.padWidthOperation),
+			$this.colorOperation
+		);
+
+		$this.WriteRowEnd(
+			$this.columnNameStatus.PadRight($this.padWidthStatus),
+			$this.colorTabelColumnName,
+			$this.columnNameTime.PadRight($this.padWidthTime),
+			$this.colorTabelColumnName,
+			$this.columnNameDetails,
+			$this.colorTabelColumnName
+		);
+
+		# table head bottom
+
+		$tableHeadBottom = '┣' + $this.tableFillId + '╋' + $this.tableFillOperation + '╋' + $this.tableFillStatus + '╋' + $this.tableFillTime + '╋' + $this.tableFillDetails;
+
+		Write-Host $tableHeadBottom -ForegroundColor $this.colorTabelBorder;
+	}
+
+	[Void] ProcessEnd(
+	)
+	{
+		$tableFootBottom = '┗' + $this.tableFillId + '┻' + $this.tableFillOperation + '┻' + $this.tableFillStatus + '┻' + $this.tableFillTime + '┻' + $this.tableFillDetails;
+
+		Write-Host $tableFootBottom -ForegroundColor $this.colorTabelBorder;
+	}
+
+	hidden [Void] WriteRowBegin(
+		[String] $id,
+		[ConsoleColor] $idColor,
+		[String] $message,
+		[ConsoleColor] $messageColor
+	)
+	{
+		Write-Host '┃ ' -ForegroundColor $this.colorTabelBorder -NoNewline;
+
+		Write-Host $id -ForegroundColor $idColor -NoNewline;
+
+		Write-Host ' ┃ ' -ForegroundColor $this.colorTabelBorder -NoNewline;
+
+		Write-Host $message -ForegroundColor $messageColor -NoNewline;
+
+		Write-Host ' ┃ ' -ForegroundColor $this.colorTabelBorder -NoNewline;
+	}
+
+	[Void] WriteRowEnd(
+		[String] $status,
+		[ConsoleColor] $statusColor,
+		[String] $duration,
+		[ConsoleColor] $durationColor,
+		[String] $details,
+		[ConsoleColor] $detailsColor
+	)
+	{
+		Write-Host $status -ForegroundColor $statusColor -NoNewline;
+
+		Write-Host ' ┃ ' -ForegroundColor $this.colorTabelBorder -NoNewline;
+
+		Write-Host $duration -ForegroundColor $durationColor -NoNewline;
+
+		Write-Host ' ┃ ' -ForegroundColor $this.colorTabelBorder -NoNewline;
+
+		Write-Host $details -ForegroundColor $detailsColor;
+	}
+}
--- tests/PowerPlatform/Environment/Solution.ps1
+++ tests/PowerPlatform/Environment/Solution.ps1
@@ -17,142 +17,142 @@ param
 	[Parameter(Mandatory = $false)] [ValidateNotNullOrEmpty()] [String]  $solutionV2File = 'Solution\SolutionManageTest_1_0_0_3_managed.zip',
 	[Parameter(Mandatory = $false)]                            [Boolean] $isVerbose = $false
 )
-
-# get current script directory
-$invocationDirectory = Split-Path $script:MyInvocation.MyCommand.Path;
-
-# import PowerShell module: Helpers
-# Import-Module (Join-Path $invocationDirectory '..\..\..\sources\Helpers.psm1') -NoClobber -Force;
-
-Import-Module 'C:\Users\stass\Sources\github-stas-sultanov\ms-ps-modules\sources\Helpers.psm1' -NoClobber -Force;
-
-# improt PowerShell module: Power Platform
-Import-Module (Join-Path $invocationDirectory '..\..\..\sources\PowerPlatform\PowerPlatform.psd1') -NoClobber -Force;
-
-# disable annoying Az warnings
-$null = Update-AzConfig -DisplayBreakingChangeWarning $false;
-
-$log = New-ConsoleLogHelper 38;
-
-Write-Host 'Process Begin' -ForegroundColor Cyan;
-
-# STEP 00
-
-$log.OperationBegin('Get Envirnomnent access token');
-
-$environmentAccessToken = (Get-AzAccessToken -ResourceUrl $environmentUrl -AsSecureString).Token;
-
-$log.OperationEnd( ($null -ne $environmentAccessToken) -and (0 -lt $environmentAccessToken.Length), "environmentUrl: $($environmentUrl)" );
-
-# STEP 01
-
-$log.OperationBegin('Solution Stage');
-
-$importStageInfo = PowerPlatform.Solution.Stage `
-	-accessToken $environmentAccessToken `
-	-customizationFile (Join-Path $invocationDirectory $solutionV1File) `
-	-environmentUrl $environmentUrl;
-
-$log.OperationEnd($importStageInfo.success, ($importStageInfo.success ? "version: $($importStageInfo.versionCurrent), uploadId: $($importStageInfo.uploadId)" : $null));
-
-# STEP 02
-
-$log.OperationBegin('Solution Check if ready for Import');
-
-$noPreviousVersion = $null -eq $importStageInfo.versionPrevious;
-
-$log.OperationEnd($noPreviousVersion, ($noPreviousVersion ? $null : "versionPrevious: $($importStageInfo.versionPrevious)"));
-
-if (-not $noPreviousVersion)
+process
 {
-	Write-Host 'Process Abort' -ForegroundColor Red;
+	# get current script directory
+	$invocationDirectory = Split-Path $script:MyInvocation.MyCommand.Path;
 
-	return;
+	# import PowerShell module: Helpers
+	Import-Module (Join-Path $invocationDirectory '..\..\..\sources\Helpers.psm1') -NoClobber -Force;
+
+	# improt PowerShell module: Power Platform
+	Import-Module (Join-Path $invocationDirectory '..\..\..\sources\PowerPlatform\PowerPlatform.psd1') -NoClobber -Force;
+
+	# disable annoying Az warnings
+	$null = Update-AzConfig -DisplayBreakingChangeWarning $false;
+
+	$log = New-ConsoleLogHelper 16;
+
+	$log.ProcessBegin();
+
+	# PROCESS BEGIN
+
+	$log.OperationBegin('Get Access Token');
+
+	$environmentAccessToken = (Get-AzAccessToken -ResourceUrl $environmentUrl -AsSecureString).Token;
+
+	$log.OperationEnd( ($null -ne $environmentAccessToken) -and (0 -lt $environmentAccessToken.Length) );
+
+	# STEP 01
+
+	$log.OperationBegin('Import Stage');
+
+	$importStageInfo = PowerPlatform.Solution.Stage `
+		-accessToken $environmentAccessToken `
+		-customizationFile (Join-Path $invocationDirectory $solutionV1File) `
+		-environmentUrl $environmentUrl;
+
+	$log.OperationEnd($importStageInfo.success, ($importStageInfo.success ? "version: $($importStageInfo.versionCurrent), uploadId: $($importStageInfo.uploadId)" : $null));
+
+	# STEP 02
+
+	$log.OperationBegin('Import Check');
+
+	$noPreviousVersion = [String]::IsNullOrWhiteSpace($importStageInfo.versionPrevious);
+
+	$log.OperationEnd($noPreviousVersion, ($noPreviousVersion ? $null : "versionPrevious: $($importStageInfo.versionPrevious)"));
+
+	if (-not $noPreviousVersion)
+	{
+		Write-Host 'Process Abort' -ForegroundColor Red;
+
+		return;
+	}
+
+	# STEP 03
+
+	$log.OperationBegin('Import Async');
+
+	$importAsyncOperationId = PowerPlatform.Solution.ImportAsync `
+		-accessToken $environmentAccessToken `
+		-environmentUrl $environmentUrl `
+		-importJobId ([Guid]::NewGuid()) `
+		-overwriteUnmanagedCustomizations $false `
+		-publishWorkflows $false `
+		-stageSolutionUploadId $importStageInfo.uploadId;
+
+	$log.OperationEndSuccess("asyncOperationId: $($importAsyncOperationId)");
+
+	# STEP 04
+
+	$log.OperationBegin('Import Await');
+
+	$importAwaitResult = PowerPlatform.AsyncOperation.Await `
+		-accessToken $environmentAccessToken `
+		-asyncOperationId $importAsyncOperationId `
+		-environmentUrl $environmentUrl;
+
+	$log.OperationEnd($importAwaitResult);
+
+	# STEP 05
+
+	$log.OperationBegin('Upgrade Stage');
+
+	$upgradeStageInfo = PowerPlatform.Solution.Stage `
+		-accessToken $environmentAccessToken `
+		-customizationFile (Join-Path $invocationDirectory $solutionV2File) `
+		-environmentUrl $environmentUrl;
+
+	$log.OperationEnd($upgradeStageInfo.success, $upgradeStageInfo.success ? "version: $($upgradeStageInfo.versionCurrent), uploadId: $($upgradeStageInfo.uploadId)" : $null);
+
+	# STEP 06
+
+	$log.OperationBegin('Upgrade Async');
+
+	$upgradeAsyncOperationId = PowerPlatform.Solution.StageAndUpgradeAsync `
+		-accessToken $environmentAccessToken `
+		-environmentUrl $environmentUrl `
+		-importJobId ([Guid]::NewGuid()) `
+		-overwriteUnmanagedCustomizations $false `
+		-publishWorkflows $false `
+		-stageSolutionUploadId $upgradeStageInfo.uploadId;
+
+	$log.OperationEndSuccess("asyncOperationId: $($upgradeAsyncOperationId)");
+
+	# STEP 07
+
+	$log.OperationBegin('Upgrade Await');
+
+	$upgradeAwaitResult = PowerPlatform.AsyncOperation.Await `
+		-accessToken $environmentAccessToken `
+		-asyncOperationId $upgradeAsyncOperationId `
+		-environmentUrl $environmentUrl;
+
+	$log.OperationEnd($upgradeAwaitResult);
+
+	# STEP 08
+
+	$log.OperationBegin('Uninstall Async');
+
+	$uninstallAsyncOperationId = PowerPlatform.Solution.UninstallAsync `
+		-accessToken $environmentAccessToken `
+		-environmentUrl $environmentUrl `
+		-solutionUniqueName $solutionUniqueueName;
+
+	$log.OperationEndSuccess("asyncOperationId: $($uninstallAsyncOperationId)");
+
+	# STEP 09
+
+	$log.OperationBegin('Uninstall Await');
+
+	$uninstallAwaitResult = PowerPlatform.AsyncOperation.Await `
+		-accessToken $environmentAccessToken `
+		-asyncOperationId $uninstallAsyncOperationId `
+		-environmentUrl $environmentUrl;
+
+	$log.OperationEnd($uninstallAwaitResult);
+
+	# PROCESS END
+
+	$log.ProcessEnd();
 }
-
-# STEP 03
-
-$log.OperationBegin('Solution Import Async');
-
-$importAsyncOperationId = PowerPlatform.Solution.ImportAsync `
-	-accessToken $environmentAccessToken `
-	-environmentUrl $environmentUrl `
-	-importJobId ([Guid]::NewGuid()) `
-	-overwriteUnmanagedCustomizations $false `
-	-publishWorkflows $false `
-	-stageSolutionUploadId $importStageInfo.uploadId;
-
-$log.OperationEndSuccess("asyncOperationId: $($importAsyncOperationId)");
-
-# STEP 04
-
-$log.OperationBegin('Solution Import Await');
-
-$importAwaitResult = PowerPlatform.AsyncOperation.Await `
-	-accessToken $environmentAccessToken `
-	-asyncOperationId $importAsyncOperationId `
-	-environmentUrl $environmentUrl;
-
-$log.OperationEnd($importAwaitResult);
-
-# STEP 05
-
-$log.OperationBegin('Solution Upgrade Stage');
-
-$upgradeStageInfo = PowerPlatform.Solution.Stage `
-	-accessToken $environmentAccessToken `
-	-customizationFile (Join-Path $invocationDirectory $solutionV2File) `
-	-environmentUrl $environmentUrl;
-
-$log.OperationEnd($upgradeStageInfo.success, $upgradeStageInfo.success ? "version: $($upgradeStageInfo.versionCurrent), uploadId: $($upgradeStageInfo.uploadId)" : $null);
-
-# STEP 06
-
-$log.OperationBegin('Solution Upgrade Async');
-
-$upgradeAsyncOperationId = PowerPlatform.Solution.StageAndUpgradeAsync `
-	-accessToken $environmentAccessToken `
-	-environmentUrl $environmentUrl `
-	-importJobId ([Guid]::NewGuid()) `
-	-overwriteUnmanagedCustomizations $false `
-	-publishWorkflows $false `
-	-stageSolutionUploadId $upgradeStageInfo.uploadId;
-
-$log.OperationEndSuccess("asyncOperationId: $($upgradeAsyncOperationId)");
-
-# STEP 07
-
-$log.OperationBegin('Solution Upgrade Await');
-
-$upgradeAwaitResult = PowerPlatform.AsyncOperation.Await `
-	-accessToken $environmentAccessToken `
-	-asyncOperationId $upgradeAsyncOperationId `
-	-environmentUrl $environmentUrl;
-
-$log.OperationEnd($upgradeAwaitResult);
-
-# STEP 08
-
-$log.OperationBegin('Solution Uninstall Async');
-
-$uninstallAsyncOperationId = PowerPlatform.Solution.UninstallAsync `
-	-accessToken $environmentAccessToken `
-	-environmentUrl $environmentUrl `
-	-solutionUniqueName $solutionUniqueueName;
-
-$log.OperationEndSuccess("asyncOperationId: $($uninstallAsyncOperationId)");
-
-# STEP 09
-
-$log.OperationBegin('Solution Uninstall Await');
-
-$uninstallAwaitResult = PowerPlatform.AsyncOperation.Await `
-	-accessToken $environmentAccessToken `
-	-asyncOperationId $uninstallAsyncOperationId `
-	-environmentUrl $environmentUrl;
-
-$log.OperationEnd($uninstallAwaitResult);
-
-<# end #>
-
-Write-Host 'Complete.' -ForegroundColor Blue;
